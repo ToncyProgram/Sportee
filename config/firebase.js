147776c267281270d6f1082a5b8a96482ba1ce5b
ToncyProgramer <Antoniokovacic1510@gmail.com>
2023-01-17 20:36:30 +0100

    initial commit

--- config/firebase.js
+++ config/firebase.js
@@ -20,6 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
 export { app, auth };
